4e7262b69593aa6ac71d9d473869e092aad655af
jennyching <jklife3@gmail.com>
2015-10-03 12:50:13 +0800

    Modify recursive algorithm

--- RecursiveCode.js
+++ RecursiveCode.js
@@ -13,3 +13,20 @@ println("The value of 0! is " + factorial(0));
 
 //Program.assertEqual(factorial(5), 120);
 //Program.assertEqual(factorial(0), 1);
+
+//second approach
+
+var factorial2 = function(n) {
+	// base case: 
+	if (n === 0){
+	return 1;
+	}
+	// recursive case:
+	return n*(factorial2(n-1));
+}; 
+
+println("The value of 0! is " + factorial(0) + ".");
+println("The value of 5! is " + factorial(5) + ".");
+
+Program.assertEqual(factorial(0), 1);
+Program.assertEqual(factorial(5), 120);
