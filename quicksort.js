e7b914fddb4a151a57bb113e7192dd5546b8817f
jennyching <jklife3@gmail.com>
2015-11-01 15:50:42 +0800

    Upload quickSort

--- quicksort.js
+++ quicksort.js
@@ -8,10 +8,19 @@ var partition=function(array, p, r){
 
 
 var quickSort = function(array, p, r) {
-    if(
+    if(p < r) {
+        var pivot = partition(array, p, r);
+        quickSort(array, p, pivot-1);
+        quickSort(array, pivot+1, r);
+    }
 };
 
 var array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6];
 quickSort(array, 0, array.length-1);
 println("Array after sorting: " + array);
-// Program.assertEqual(array, [2,3,5,6,7,9,10,11,12,14]);
+Program.assertEqual(array, [2,3,5,6,7,9,10,11,12,14]);
+
+var array2 = [9, 7, 5, 0, 12, 2, 14, 3, 10, 6];
+quickSort(array2, 0, array2.length-1);
+println("Array after sorting: " + array2);
+Program.assertEqual(array2, [0,2,3,5,6,7,9,10,12,14]);
